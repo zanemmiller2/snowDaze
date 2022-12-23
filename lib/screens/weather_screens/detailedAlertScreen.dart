429b8b66f3e48a932e7f3db0002a27a63f94d70c
zanemmiller2 <zanemmiller2@gmail.com>
2022-12-23 14:53:22 -0800

    added template pages for detailed grid tile ontap and text page for weather alert details.

---
+++ lib/screens/weather_screens/detailedAlertScreen.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+
+class DetailedAlertScreen extends StatelessWidget {
+  final Map detailedLocationForecastDataAlerts;
+  final String effectEndTime;
+  final String effectStartTime;
+  const DetailedAlertScreen({super.key, required this.detailedLocationForecastDataAlerts, required this.effectStartTime, required this.effectEndTime});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: const Text('Severe Weather Alert'),
+      ),
+      body: SingleChildScrollView (
+        padding: EdgeInsets.all(10.0),
+        child: Column (
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Flexible(
+                fit: FlexFit.loose,
+                child: Text(
+                  '${detailedLocationForecastDataAlerts['event']}',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                )),
+            // In effect until ...
+            Flexible(
+              fit: FlexFit.loose,
+              child: Text(
+                'In effect from $effectStartTime\n until $effectEndTime',
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            // Sender
+            Flexible(
+                fit: FlexFit.loose,
+                child: Text(
+                  '${detailedLocationForecastDataAlerts['sender_name']}',
+                  style: Theme.of(context).textTheme.bodySmall,
+                )),
+            SizedBox(height: 25.0),
+            Text(detailedLocationForecastDataAlerts['description'].replaceAll('\n', ''))
+        ]),
+      )
+    );
+  }
+}
---
+++ lib/screens/weather_screens/gridViewDetailScreen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class GridDetailView extends StatelessWidget {
+  const GridDetailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GRID VIEW DETAIL PAGE'),
+      ),
+      body: const Text('DETAIL GRID VIEW GOES HERE'),
+    );
+  }
+
+}
